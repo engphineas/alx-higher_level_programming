8c4a9adbfa7bd1e869414712f99633c66e2569be
engphineas <phineastheengineer@gmail.com>
2023-12-07 05:07:21 -0800

    data_struts

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix = []):
+def square_matrix_simple(matrix=[]):
     new_matrix = [[entry ** 2 for entry in row] for row in matrix]
     return new_matrix
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,8 +2,8 @@
 def roman_to_int(roman_string: str):
     if roman_string is None or type(roman_string) != str:
         return 0
-    data_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    nums = [data_values[x] for x in roman_string] + [0]
+    d_value = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    nums = [d_value[x] for x in roman_string] + [0]
     figure = 0
 
     for i in range(len(nums) - 1):
