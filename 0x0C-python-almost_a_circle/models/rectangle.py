35df260a9f5e72d082115fd4bd65a52b7bfeb4f0
engphineas <phineastheengineer@gmail.com>
2024-01-16 12:23:57 -0800

    python_circle_model

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -3,7 +3,7 @@
 from models.base import Base
 
 
-classs Rectangle(Base):
+class Rectangle(Base):
     """Representation of a Rectangle."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
